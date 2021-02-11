aced82e9ff5df6b1f724b83f1cf0a4bb7220f0df
funny0619 <sunnyb@hku.hk>
2021-02-11 15:24:00 +0900

    added reset button

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -3,11 +3,12 @@ window.onload = function() {
 }
 var turn = 0;
 var square = document.getElementsByClassName("square");
+var button = document.getElementsByClassName("button")[0];
 //turn starts at 0 if O starts
 //starts at 1 if X starts
 function setup() {
     for(let i=0; i<9; ++i) {
-        square[i].addEventListener('click',function (e) {
+        square[i].addEventListener('click',function(e) {
             var result;
             if(turn % 2 == 0) {
                 e.target.textContent = "O";
@@ -18,12 +19,28 @@ function setup() {
             e.target.style.pointerEvents = "none";
             e.target.style.color = "black";
             ++turn;
-            result = end();
+            result = end()
+            if(result == 1) {
+                for(let i=0; i<9; ++i) {
+                    square[i].style.pointerEvents = "none";
+                }
+            }
         })
     }
-}
+    button.addEventListener('click',function(e) {
+        reset()
+    })
+    
+};
 
+function reset() {
+    for(let i=0; i<9;++i) {
+        square[i].textContent = "";
+        square[i].removeAttribute("style");
+    }
+}
 // 1 is end
+// find way to make win check better
 function end() {
     let one = document.getElementById("0").textContent
     let two = document.getElementById("1").textContent
@@ -34,13 +51,13 @@ function end() {
     let seven = document.getElementById("6").textContent
     let eight = document.getElementById("7").textContent
     let nine = document.getElementById("8").textContent
-    if((one == two && two == three) || (four == five && five == six) || (seven == eight && eight == nine)) {
+    if((one == two && two == three) && (one && two && three) || (four == five && five == six) && (four && five && six) || (seven == eight && eight == nine) && (seven && eight && nine)) {
         return 1;
     }
-    if((one == four && four == seven) || (two == five && five == eigth) || (three == six && six == nine)) {
+    if((one == four && four == seven) && (one && four && seven) || (two == five && five == eight) && (two && five && eight) || (three == six && six == nine) && (three && six && nine)) {
         return 1;
     }
-    if((one == five && five == nine) || (three == five && five == seven)) {
+    if((one == five && five == nine) && (one && five && nine) || (three == five && five == seven) && (three && five && seven)) {
         return 1;
     }
     return 0
